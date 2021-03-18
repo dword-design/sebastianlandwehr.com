15bb955b55fbacd7b04e6cb8d8f4b761916a2205
Sebastian Landwehr <info@dword-design.de>
2021-03-18 12:47:59 +0100

    feat: add created at date to posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,7 +5,7 @@ export default {
   asyncData: async context => ({
     posts: await context
       .$content('posts')
-      .only(['title', 'description', 'img', 'slug', 'author'])
+      .only(['title', 'description', 'img', 'slug', 'author', 'createdAt'])
       .sortBy('createdAt', 'asc')
       .fetch(),
   }),
@@ -16,22 +16,31 @@ export default {
     return (
       <section class="section">
         <article class="container">
-          <h1 class="title">Blog Posts</h1>
+          <h1 class="title">Blog</h1>
           <ul>
             {this.posts
               |> map(post => (
                 <li>
-                  <a
-                    href={
-                      this.$router.resolve({
-                        name: 'blog.slug',
-                        params: { slug: post.slug },
-                      }).href
-                    }
-                  >
-                    <h2>{post.title}</h2>
-                    <p>{post.description}</p>
-                  </a>
+                  <h2 class="title is-size-4">
+                    <a
+                      href={
+                        this.$router.resolve({
+                          name: 'blog.slug',
+                          params: { slug: post.slug },
+                        }).href
+                      }
+                    >
+                      {post.title}
+                    </a>
+                  </h2>
+                  <div class="subtitle is-size-7">
+                    {new Date(post.createdAt).toLocaleDateString('en', {
+                      day: 'numeric',
+                      month: 'short',
+                      year: 'numeric',
+                    })}
+                  </div>
+                  <p>{post.description}</p>
                 </li>
               ))}
           </ul>
