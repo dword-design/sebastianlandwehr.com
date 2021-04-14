6ab959a0192967bbafb0ba7b2722e171aeb2465b
dword <info@dword-design.de>
2021-04-14 21:41:26 +0000

    fix: blog images

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,11 +1,12 @@
 <script>
 import { map } from '@dword-design/functions'
+import { format } from 'date-fns'
 
 export default {
   asyncData: async context => ({
     posts: await context
       .$content('posts')
-      .only(['title', 'description', 'img', 'slug', 'author', 'createdAt'])
+      .only(['title', 'description', 'slug', 'createdAt'])
       .sortBy('createdAt', 'desc')
       .fetch(),
   }),
@@ -15,36 +16,51 @@ export default {
   render() {
     return (
       <main class="section">
-        <article class="container">
+        <div class="container is-content">
           <h1 class="title">Blog</h1>
-          <ul>
+          <div class="tile is-ancestor is-vertical">
             {this.posts
               |> map(post => (
-                <li>
-                  <h2 class="title is-size-4">
-                    <a
-                      href={
-                        this.$router.resolve({
-                          name: 'blog.slug',
-                          params: { slug: post.slug },
-                        }).href
-                      }
-                    >
-                      {post.title}
-                    </a>
-                  </h2>
-                  <div class="subtitle is-size-7">
-                    {new Date(post.createdAt).toLocaleDateString('en', {
-                      day: 'numeric',
-                      month: 'short',
-                      year: 'numeric',
-                    })}
-                  </div>
-                  <p>{post.description}</p>
-                </li>
+                <div class="tile is-parent">
+                  <article class="card tile is-child">
+                    <header class="card-image">
+                      <figure class="image is-devto-banner">
+                        <img
+                          alt={`Cover image for ${post.title}`}
+                          src={`/blog/${post.slug}/banner.png`}
+                        />
+                      </figure>
+                    </header>
+                    <div class="card-content">
+                      <h2 class="title is-size-4">
+                        <a
+                          class="boxlink"
+                          href={
+                            this.$router.resolve({
+                              name: 'blog.slug',
+                              params: { slug: post.slug },
+                            }).href
+                          }
+                        >
+                          {post.title}
+                        </a>
+                      </h2>
+                      <div class="subtitle is-size-7">
+                        <time
+                          datetime={format(
+                            new Date(post.createdAt),
+                            'yyyy-MM-dd'
+                          )}
+                        >
+                          {format(new Date(post.createdAt), 'PP')}
+                        </time>
+                      </div>
+                    </div>
+                  </article>
+                </div>
               ))}
-          </ul>
-        </article>
+          </div>
+        </div>
       </main>
     )
   },
