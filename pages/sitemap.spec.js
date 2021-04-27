fb6f1da1801e2637adff8abc109687171471d536
dword <info@dword-design.de>
2021-04-27 01:27:39 +0000

    fix: test sitemap

---
+++ pages/sitemap.spec.js
@@ -0,0 +1,20 @@
+import nuxtConfig from '@dword-design/base-config-app/dist/nuxt.config'
+import { property } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+import testerPluginNuxt from '@dword-design/tester-plugin-nuxt'
+import axios from 'axios'
+import pretty from 'pretty'
+
+export default tester(
+  {
+    async init() {
+      expect(
+        axios.get('http://localhost:3000/sitemap.xml')
+          |> await
+          |> property('data')
+          |> pretty
+      ).toMatchSnapshot(this)
+    },
+  },
+  [testerPluginNuxt(nuxtConfig)]
+)
