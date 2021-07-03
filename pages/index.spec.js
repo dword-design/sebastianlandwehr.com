bcc4233cfffdf44c02d1d56569d0218bb6b01131
dword <info@sebastianlandwehr.com>
2021-07-03 23:13:08 +0000

    chore: remove unused import

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -2,7 +2,6 @@ import nuxtConfig from '@dword-design/base-config-app/dist/nuxt.config'
 import tester from '@dword-design/tester'
 import testerPluginNuxt from '@dword-design/tester-plugin-nuxt'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
-import { delay } from '@dword-design/functions'
 
 export default tester(
   {
