0ea9829646e57577440444f553cdba7fca47adc5
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-02-09 10:16:36 +0000

    chore: accept all cookies in tests

--- pages/blog/_slug.spec.js
+++ pages/blog/_slug.spec.js
@@ -1,4 +1,5 @@
 import nuxtConfig from '@dword-design/base-config-nuxt/dist/nuxt.config'
+import { delay } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginNuxt from '@dword-design/tester-plugin-nuxt'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
@@ -13,6 +14,12 @@ export default tester(
         height: 1,
         width: 1400,
       })
+
+      const acceptAllCookiesButton = await this.page.waitForXPath(
+        "//button/span[text()='Accept all cookies']/.."
+      )
+      await acceptAllCookiesButton.click()
+      await delay(500)
       expect(
         await this.page.screenshot({ fullPage: true })
       ).toMatchImageSnapshot(this)
