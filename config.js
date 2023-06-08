fb23a139ba1c8d491037745ab9203af8810d3084
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-06-08 16:48:22 +0000

    fix: foo

--- config.js
+++ config.js
@@ -6,39 +6,6 @@ import { appName, appTitle } from './model/variables.js'
 
 export default {
   css: ['@/assets/style.scss'],
-  /* feed: [
-    {
-      create: async feed => {
-        const $content = require('@nuxt/content').$content
-        feed.options = {
-          description: appTitle,
-          link: `${process.env.BASE_URL}/blog`,
-          title: appName,
-        }
-
-        const posts = await $content('blog')
-          .sortBy('createdAt', 'desc')
-          .fetch()
-        for (const post of posts) {
-          const url = `${process.env.BASE_URL}/blog/${post.slug}`
-          feed.addItem({
-            author: post.authors,
-            content: endent`
-              <p><img alt="Cover image" src="${process.env.BASE_URL}/blog/${post.slug}/banner.png"></p>
-              ${post.bodyHtml}
-            `,
-            date: new Date(post.createdAt),
-            description: post.description,
-            id: url,
-            link: url,
-            title: post.title,
-          })
-        }
-      },
-      path: '/feed',
-      type: 'rss2',
-    },
-  ], */
   head: {
     link: [
       {
@@ -88,19 +55,6 @@ export default {
       },
     }],
     'nuxt-content-git',
-    /*[
-      'nuxt-content-body-html',
-      {
-        highlighter: undefined,
-        rehypePlugins: [
-          [
-            packageName`rehype-urls`,
-            url => (url.host ? url : new URL(url.href, process.env.BASE_URL)),
-          ],
-        ],
-      },
-    ], */
-    // '@nuxtjs/feed',
     ['nuxt-gtag', { gtag: { id: process.env.GOOGLE_ANALYTICS_ID } }],
   ],
   name: appName,
---
+++ server/routes/feed.get.js
@@ -0,0 +1,34 @@
+import { serverQueryContent } from '#content/server'
+import { defineEventHandler } from '#imports'
+import { Feed } from 'feed'
+import { endent } from '@dword-design/functions'
+import { appName, appTitle } from '@/model/variables.js'
+
+export default defineEventHandler(async event => {
+  const posts = await serverQueryContent(event).sort({ 'createdAt': -1 }).find()
+
+  const feed = new Feed({
+    description: appTitle,
+    link: `${process.env.BASE_URL}/blog`,
+    title: appName,
+  })
+
+  for (const post of posts) {
+    const url = `${process.env.BASE_URL}${post._path}`
+    feed.addItem({
+      author: post.authors,
+      content: endent`
+        <p><img alt="Cover image" src="${process.env.BASE_URL}/${post._path}/banner.png"></p>
+        ${post.bodyHtml}
+      `,
+      date: new Date(post.createdAt),
+      description: post.description,
+      id: url,
+      link: url,
+      title: post.title,
+    })
+  }
+  
+  event.res.setHeader('content-type', 'application/rss+xml')
+  event.res.end(feed.rss2())
+})
