6bbce6c899ad803b67bb94cb7762df1633d737c8
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-06-09 10:03:21 +0000

    fix: fix blog footer

--- model/blog-footer.js
+++
@@ -1,1 +0,0 @@
-export default "**If you like what I'm doing, follow me on [Twitter](https://twitter.com/seblandwehr) or check out my [website](https://sebastianlandwehr.com). Also consider donating at [Buy Me a Coffee](https://www.buymeacoffee.com/dword), [PayPal](https://www.paypal.com/paypalme/SebastianLandwehr) or [Patreon](https://www.patreon.com/dworddesign). Thank you so much! ❤️**"
