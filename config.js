53af03e40ad9936113d24436e19ea4d3f325f526
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-19 08:27:10 +0000

    fix: foo

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ const resolver = createResolver(import.meta.url);
 
 if (process.env.CODESPACE_NAME) {
   process.env.BASE_URL = `https://${process.env.CODESPACE_NAME}-${process.env.PORT}.app.github.dev`;
+  process.env.HOST = '0.0.0.0';
 }
 
 export default {
@@ -60,9 +61,6 @@ export default {
   ogImage: `${process.env.BASE_URL}/images/og-image.png`,
   router: { options: { linkActiveClass: 'is-active' } },
   runtimeConfig: { public: { gtag: { id: process.env.GOOGLE_ANALYTICS_ID } } },
-  ...process.env.CODESPACE_NAME && { server: {
-    host: '0.0.0.0',
-  } },
   site: { url: process.env.BASE_URL },
   title: appTitle,
 };
