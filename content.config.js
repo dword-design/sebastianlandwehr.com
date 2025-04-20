857e684b96abcfca44fd75067e2e2ca86fb37a63
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-20 18:48:03 +0000

    fix: sitemap

--- content.config.js
+++ content.config.js
@@ -1,11 +1,14 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content';
+import { asSitemapCollection } from '@nuxtjs/sitemap/content';
 
 export default defineContentConfig({
   collections: {
-    blog: defineCollection({
-      schema: z.object({ createdAt: z.date(), updatedAt: z.date() }),
-      source: 'blog/*.md',
-      type: 'page',
-    }),
+    blog: defineCollection(
+      asSitemapCollection({
+        schema: z.object({ createdAt: z.date(), updatedAt: z.date() }),
+        source: 'blog/*.md',
+        type: 'page',
+      }),
+    ),
   },
 });
