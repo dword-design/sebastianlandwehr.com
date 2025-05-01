db0dd95a5f3cfb81cf03e21eb0641e8b26b86a4e
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-01 22:21:42 +0000

    fix: foo

--- nuxt.config.js
+++
@@ -1,6 +0,0 @@
-import config from '@dword-design/base-config-nuxt/config';
-
-export default {
-  extends: ['@dword-design/base-config-nuxt/nuxt.config'],
-  ...config,
-};
--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from '@playwright/test';
-import { isCI } from 'std-env';
 
 export default defineConfig({
   snapshotPathTemplate:
     '{snapshotDir}/{testFileDir}/{testFileName}-snapshots/{arg}{-projectName}{ext}',
 
-  use: { trace: 'retain-on-failure' },
   webServer: {
     command: 'base dev',
-    reuseExistingServer: !isCI,
     stdout: 'pipe',
     timeout: 120000,
     url: 'http://localhost:3000',
