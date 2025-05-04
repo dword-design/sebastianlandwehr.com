dc8efc4a18b1f88814e32c8a97824e19e7b51c4a
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-04 03:53:20 +0000

    fix: foo

--- global-setup.js
+++ global-setup.js
@@ -6,6 +6,6 @@ import pid from './global-pid.js';
 export default async () => {
   await execaCommand('nuxt-babel build', { stdio: 'inherit' });
   const nuxt = execaCommand('base start', { stderr: 'inherit' });
-  await portReady(3000);
   pid.value = nuxt.pid;
+  await portReady(3000);
 };
